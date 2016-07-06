63a83c11dc9f564045435c50805efecff0989df2
timbu <tim.buckland@student.com>
2016-07-06 23:27:00 +0100

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,28 +1,5 @@
-# Nameko relies on eventlet
-# You should monkey patch the standard library as early as possible to avoid
-# importing anything before the patch is applied.
-# See http://eventlet.net/doc/patching.html#monkeypatching-the-standard-library
 import eventlet
 eventlet.monkey_patch()  # noqa
 
 from nameko.containers import ServiceContainer
 import pytest
-
-
-@pytest.yield_fixture
-def container_factory():
-
-    all_containers = []
-
-    def make_container(service_cls, config, worker_ctx_cls=None):
-        container = ServiceContainer(service_cls, config, worker_ctx_cls)
-        all_containers.append(container)
-        return container
-
-    yield make_container
-
-    for c in all_containers:
-        try:
-            c.stop()
-        except:
-            pass
