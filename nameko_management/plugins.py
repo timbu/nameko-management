aad5c9b229ad5790fa0fc2de9c108085887c8ed7
timbu <tim.buckland@student.com>
2016-07-27 23:37:30 +0100

    beginnings of ListEntrypoints

--- nameko_management/plugins.py
+++ nameko_management/plugins.py
@@ -1,3 +1,4 @@
+import inspect
 import json
 from logging import getLogger
 
@@ -87,3 +88,26 @@ class HealthcheckPlugin(ManagementPlugin):
             except AttributeError:
                 pass
         return result
+
+
+@register_plugin
+class ListEntrypoints(ManagementPlugin):
+    name = 'entrypoints'
+
+    def get_result(self, request):
+        results = []
+        service_cls = self.container.service_cls
+        for ep in self.container.entrypoints:
+            method = getattr(service_cls, ep.method_name)
+            # TODO - customise info for each entrypoint type
+            # URL rules for http, sensitive vars for rpc, queue names for
+            # consumers etc?
+            results.append({
+                'type': type(ep).__name__,
+                'name': ep.method_name,
+                # TODO - make signature more readable
+                'signature': inspect.getfullargspec(method),
+                'doc': inspect.getdoc(method),
+                # TODO 'expected_exceptions'?
+            })
+        return results
